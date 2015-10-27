c7667a2d7c06a3f2619ac9097bb932bb81d68108
Justas T <j.trimailovas@gmail.com>
2015-10-27 23:17:26 +0200

    Create Receipt model

--- table/models.py
+++ table/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import fields
 
-# Create your models here.
+
+class Receipt(models.Model):
+    title = fields.CharField(max_length=255)
+    price = fields.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(User)
+
+    def __str__(self):
+        return "{}_{}".format(self.title, self.price)
