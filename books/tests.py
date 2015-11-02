3ee458bc93c8143d5ed214ce1cde438d5cd04c70
Justas T <j.trimailovas@gmail.com>
2015-11-02 18:10:51 +0200

    Create tests

--- books/tests.py
+++ books/tests.py
@@ -1,12 +1,40 @@
+from django.core.urlresolvers import reverse
+from django.test import Client
 from django.test import TestCase
 
 from books.factories import ReceiptFactory
+from books.factories import UserFactory
 from books.models import Receipt
 
 
 class ReceiptTests(TestCase):
-    def test_create(self):
+    def setUp(self):
+        self.user = UserFactory()
+
+    def test_create_model(self):
         self.assertEqual(0, Receipt.objects.count())
         ReceiptFactory()
         self.assertEqual(1, Receipt.objects.count())
-        self.assertEqual('receipt0', Receipt.objects.latest('id').title)
+        self.assertEqual(str(Receipt.objects.latest('id')), 'receipt0_10.00')
+
+    def test_receipt_create_get(self):
+        c = Client()
+        logged_in = c.login(username=self.user.username, password='secret')
+        self.assertTrue(logged_in)
+
+        response = c.get(reverse('receipt_create'))
+        self.assertEqual(200, response.status_code)
+
+    def test_receipt_create_post(self):
+        c = Client()
+        logged_in = c.login(username=self.user.username, password='secret')
+        self.assertTrue(logged_in)
+
+        self.assertEqual(0, Receipt.objects.count())
+        response = c.post(reverse('receipt_create'),
+                          {'title': 'forty-two', 'price': 42},
+                          follow=True)
+        self.assertRedirects(response, reverse('receipt_list'))
+        self.assertEqual(1, Receipt.objects.count())
+        self.assertEqual('forty-two', Receipt.objects.latest('id').title)
+        self.assertEqual(42, Receipt.objects.latest('id').price)
--- finance/tests.py
+++ finance/tests.py
@@ -5,28 +5,19 @@ from django.test import TestCase
 from accounts.factories import UserFactory
 
 
-class LoginTests(TestCase):
+class HomePageTests(TestCase):
     def setUp(self):
         self.user = UserFactory()
 
-    def test_login_get(self):
+    def test_home_not_logged_in(self):
         c = Client()
-        response = c.get(reverse('login'))
+        response = c.get(reverse('home'))
         self.assertEqual(200, response.status_code)
 
-    def test_login_post(self):
-        c = Client()
-        response = c.post(
-            reverse('login'),
-            {'username': self.user.username, 'password': 'secret'},
-            follow=True
-        )
-        self.assertRedirects(response, reverse('receipt_list'))
-
-    def test_login_redirect(self):
+    def test_home_logged_in(self):
         c = Client()
         logged_in = c.login(username=self.user.username, password='secret')
         self.assertTrue(logged_in)
 
-        response = c.get(reverse('login_redirect'))
+        response = c.get(reverse('home'))
         self.assertRedirects(response, reverse('receipt_list'))
