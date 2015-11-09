2fa120b7841c92d39a5612df0a93f725e1c910f5
Justas T <j.trimailovas@gmail.com>
2015-11-09 09:50:07 +0200

    Add provision file for production

---
+++ provision_prod.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get install -y postgresql-9.3
+sudo -u postgres createdb finance
+sudo -u postgres psql -c "CREATE USER justas CREATEDB;"
+
+sudo add-apt-repository ppa:fkrull/deadsnakes
+sudo apt-get update
+sudo apt-get install -y python3.5
+
+sudo apt-get install -y build-essential python3-pip python3.5-venv python3-psycopg2 libpq-dev python3.5-dev
+
+sudo apt-get install -y htop
