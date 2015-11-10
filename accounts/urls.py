7552f3ad2b5247ffd046569288474d81ff4f31e5
Justas T <j.trimailovas@gmail.com>
2015-11-10 19:40:05 +0200

    Create remember_me checkbox with 1 year cookie

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from accounts import views
 
 
 urlpatterns = [
-    url(r'^', include('django.contrib.auth.urls')),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^login/$', views.login, name='login'),
     url(r'^login_redirect/$', views.login_redirect, name='login_redirect'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,17 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
+
+
+def login(request, *args, **kwargs):
+    if request.method == 'POST':
+        if request.POST.get('remember_me', None):
+            # set login session cookie for 1 year
+            request.session.set_expiry(3600*24*365)
+        else:
+            request.session.set_expiry(0)
+    return auth_views.login(request, *args, **kwargs)
 
 
 @login_required
