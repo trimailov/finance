494f14a69d08e9bfd556fccc6b4e2319db129a38
Justas T <j.trimailovas@gmail.com>
2015-11-16 23:56:31 +0200

    Add created and modified fields to Receipt

--- books/models.py
+++ books/models.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import fields
+from django.utils import timezone
 
 
 class Receipt(models.Model):
     title = fields.CharField(max_length=255)
     price = fields.DecimalField(max_digits=10, decimal_places=2)
+    created = fields.DateTimeField(auto_now=True)
+    modified = fields.DateTimeField(default=timezone.now())
     user = models.ForeignKey(User)
 
     def __str__(self):
