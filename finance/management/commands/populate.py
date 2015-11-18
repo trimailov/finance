bf1398239dec06216649add083cc328bc8a1ec99
Justas T <j.trimailovas@gmail.com>
2015-11-18 23:13:15 +0200

    Create management command for dummy data

---
+++ finance/management/commands/populate.py
@@ -0,0 +1,32 @@
+import random
+
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+from django.db import IntegrityError
+
+import factory
+
+from accounts.factories import UserFactory
+from books.factories import ReceiptFactory
+
+
+class Command(BaseCommand):
+    help = "Popoulates databse with dummy data"
+
+    def handle(self, *args, **options):
+        # Factory creates simple user, so ``is_staff`` is set later
+        try:
+            admin = UserFactory(username='admin', password='asdasd')
+            admin.is_staff = True
+            admin.save()
+            print("admin user have been created successfully")
+        except IntegrityError:
+            admin = User.objects.get(username='admin')
+            print("admin user already exists")
+
+        ReceiptFactory.create_batch(
+            10,
+            price=factory.Sequence(lambda n: random.randrange(-10, 10)),
+            user=admin,
+        )
+        print("Receipts for admin created")
