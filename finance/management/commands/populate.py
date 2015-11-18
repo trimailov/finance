6482c485982fe5039574eab797b46d5f1b93bacc
Justas T <j.trimailovas@gmail.com>
2015-11-19 00:43:50 +0200

    Refactor populate script
    try/except evaluation is error prone, this way is more
    reliable.

--- finance/management/commands/populate.py
+++ finance/management/commands/populate.py
@@ -2,7 +2,6 @@ import random
 
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
-from django.db import IntegrityError
 
 import factory
 
@@ -14,19 +13,26 @@ class Command(BaseCommand):
     help = "Popoulates databse with dummy data"
 
     def handle(self, *args, **options):
-        # Factory creates simple user, so ``is_staff`` is set later
-        try:
-            admin = UserFactory(username='admin', password='asdasd')
-            admin.is_staff = True
-            admin.save()
-            print("admin user have been created successfully")
-        except IntegrityError:
-            admin = User.objects.get(username='admin')
+        if not User.objects.filter(username='admin'):
+            self.create_admin()
+        else:
+            self.admin = User.objects.get(username='admin')
             print("admin user already exists")
 
+        self.create_transactions()
+
+    def create_admin(self):
+        # Factory creates simple user, so ``is_staff`` is set later
+        self.admin = UserFactory(username='admin', password='asdasd')
+        self.admin.is_staff = True
+        self.admin.save()
+        print("admin user have been created successfully")
+
+    def create_transactions(self):
         TransactionFactory.create_batch(
             10,
-            price=factory.Sequence(lambda n: random.randrange(0, 10)),
-            user=admin,
+            amount=factory.Sequence(lambda n: random.randrange(0, 10)),
+            category=random.randrange(0, 2),  # random range from 0 to 1
+            user=self.admin,
         )
         print("Transactions for admin created")
