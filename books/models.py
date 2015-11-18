1d6fb58d0a6c1d1162e10a22ad68b841fdec834d
Justas T <j.trimailovas@gmail.com>
2015-11-18 23:27:39 +0200

    Use amount and category fields instead of price
    This is more convenient and more user friendly.. I guess.

--- books/models.py
+++ books/models.py
@@ -5,11 +5,17 @@ from django.utils import timezone
 
 
 class Transaction(models.Model):
+    CATEGORY_CHOICES = (
+        (0, 'expense'),
+        (1, 'income'),
+    )
+
     title = fields.CharField(max_length=255)
-    price = fields.DecimalField(max_digits=10, decimal_places=2)
+    amount = fields.DecimalField(max_digits=10, decimal_places=2)
+    category = fields.CharField(max_length=1, choices=CATEGORY_CHOICES)
     created = fields.DateTimeField(auto_now=True)
     modified = fields.DateTimeField(default=timezone.now)
     user = models.ForeignKey(User)
 
     def __str__(self):
-        return "{}_{}".format(self.title, self.price)
+        return "{}".format(self.title)
