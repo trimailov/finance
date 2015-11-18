89fa1b45e0568fde888565f7baeb72fd91fb12ab
Justas T <j.trimailovas@gmail.com>
2015-11-18 23:27:49 +0200

    Add migrations

---
+++ books/migrations/0004_auto_20151118_2324.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('books', '0003_auto_20151118_2314'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='transaction',
+            old_name='price',
+            new_name='amount',
+        ),
+        migrations.AddField(
+            model_name='transaction',
+            name='category',
+            field=models.CharField(choices=[(0, 'expense'), (1, 'income')], max_length=1, default=0),
+            preserve_default=False,
+        ),
+        migrations.AlterField(
+            model_name='transaction',
+            name='modified',
+            field=models.DateTimeField(default=django.utils.timezone.now),
+        ),
+    ]
