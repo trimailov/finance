858318486d281f9651e27a49549d81f779402e7d
Justas T <j.trimailovas@gmail.com>
2015-11-22 18:53:52 +0200

    Do not send stats to opbeat on tests

--- finance/settings/dev.py
+++ finance/settings/dev.py
@@ -3,3 +3,9 @@ from finance.settings import *
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+# do not send statistics to opbeat when testing
+MIDDLEWARE_CLASSES = tuple(
+    middleware for middleware in MIDDLEWARE_CLASSES
+    if middleware != "opbeat.contrib.django.middleware.OpbeatAPMMiddleware"
+)
