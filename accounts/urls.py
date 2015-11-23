5b7820b847547f6ab70e34951bb06c7e6385400f
Justas T <j.trimailovas@gmail.com>
2015-11-23 10:23:56 +0200

    It looks like uwsgi was not reloaded
    So new packages were not loaded as well

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import include, url
 
 from accounts import views
-from registration.backends.simple.views import RegistrationView
 
 
 urlpatterns = [
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^login/$', views.login, name='login'),
     url(r'^login_redirect/$', views.login_redirect, name='login_redirect'),
-    url(r'^register/$', RegistrationView.as_view(), name='register'),
+    url(r'^', include('registration.backends.simple.urls')),
 ]
