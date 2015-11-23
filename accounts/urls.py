073fbfac12be97b4237f58dbf0a56cf6cddc1359
Justas T <j.trimailovas@gmail.com>
2015-11-23 10:11:25 +0200

    another apprach for calling register url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
 
 from accounts import views
+from registration.backends.simple.views import RegistrationView
 
 
 urlpatterns = [
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^login/$', views.login, name='login'),
     url(r'^login_redirect/$', views.login_redirect, name='login_redirect'),
-    url(r'^', include('registration.backends.simple.urls')),
+    url(r'^register/$', RegistrationView.as_view(), name='register'),
 ]
