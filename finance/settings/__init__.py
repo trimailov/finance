318564f6cf9618c506dc7ff7f5c2649ec3f886f8
Justas T <j.trimailovas@gmail.com>
2015-11-23 10:48:16 +0200

    Registration app is not needed

--- finance/settings/__init__.py
+++ finance/settings/__init__.py
@@ -37,7 +37,6 @@ ALLOWED_HOSTS = ['localhost', '188.166.117.76',
 # Application definition
 
 INSTALLED_APPS = (
-    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
