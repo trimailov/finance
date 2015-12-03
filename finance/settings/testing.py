a2bc0bfaa948c28fe2568cc34597cb9dbfe8fe2b
Justas T <j.trimailovas@gmail.com>
2015-12-03 23:07:46 +0200

    Create testing settings
    Due to usage of debug toolbar and opbeat, both need to be
    disabled, as tests are run with DEBUG=False by default

---
+++ finance/settings/testing.py
@@ -0,0 +1,35 @@
+from finance.settings import *
+
+"""
+Django tests are run with DEBUG=False by default
+
+This screws up django debug toolbar middleware if we run with dev settings,
+and screws up OPBEAT if we run with prod settings
+
+Here I use settings without opbeat and django debug toolbar
+"""
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'accounts',
+    'books',
+    'finance',
+    'pipeline',
+    'widget_tweaks',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
