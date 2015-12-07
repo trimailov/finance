8ac07bfa4f1c7ec99cd1c286f2d25a4c3b4c4c81
Justas T <j.trimailovas@gmail.com>
2015-12-08 00:55:30 +0200

    Get transactions for particular user
    Otherwise we'll se transactions of other users

--- books/services.py
+++ books/services.py
@@ -6,25 +6,28 @@ from django.utils import timezone
 from books.models import Transaction
 
 
-def get_months_transactions():
+def get_months_transactions(user):
     today = timezone.now()
     first_day_of_a_month = datetime(today.year, today.month, 1,
                                     tzinfo=today.tzinfo)
-    qs = Transaction.objects.filter(created__gte=first_day_of_a_month)
+    qs = Transaction.objects.filter(created__gte=first_day_of_a_month,
+                                    user=user)
     return qs
 
 
-def get_last_months_transactions():
+def get_last_months_transactions(user):
     first_day_of_a_month = timezone.now().replace(day=1)
     last_month = first_day_of_a_month - timedelta(days=1)
     first_day_of_last_month = datetime(last_month.year, last_month.month, 1,
                                        tzinfo=last_month.tzinfo)
-    qs = Transaction.objects.filter(created__gte=first_day_of_last_month)
+    qs = Transaction.objects.filter(created__gte=first_day_of_last_month,
+                                    user=user)
     return qs
 
 
-def get_this_years_transactions():
+def get_this_years_transactions(user):
     today = timezone.now()
     first_day_of_this_year = datetime(today.year, 1, 1, tzinfo=today.tzinfo)
-    qs = Transaction.objects.filter(created__gte=first_day_of_this_year)
+    qs = Transaction.objects.filter(created__gte=first_day_of_this_year,
+                                    user=user)
     return qs
