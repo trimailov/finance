ed2c56cd044f905c4325f42b4e9cf7a5df913bfd
Justas T <j.trimailovas@gmail.com>
2015-12-07 23:42:33 +0200

    Set created time with default callback
    auto_now is evil, as any editing and overriding is
    almost completely impossible (e.g. unittesting)

--- books/models.py
+++ books/models.py
@@ -15,7 +15,7 @@ class Transaction(models.Model):
     title = fields.CharField(max_length=255)
     amount = fields.DecimalField(max_digits=10, decimal_places=2)
     category = fields.CharField(max_length=3, choices=CATEGORY_CHOICES)
-    created = fields.DateTimeField(auto_now=True)
+    created = fields.DateTimeField(default=timezone.now, editable=False)
     modified = fields.DateTimeField(default=timezone.now)
     user = models.ForeignKey(User)
 
