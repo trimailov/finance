a5df33b3951b89911dd61197dae5142479e83849
Justas T <j.trimailovas@gmail.com>
2015-12-08 08:16:26 +0200

    Create transactions in more random creation times

--- finance/management/commands/populate.py
+++ finance/management/commands/populate.py
@@ -1,9 +1,14 @@
+from datetime import datetime
+from datetime import timedelta
+
 import random
 
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
 import factory
+import pytz
 
 from accounts.factories import UserFactory
 from books.factories import TransactionFactory
@@ -30,11 +35,67 @@ class Command(BaseCommand):
         self.admin.save()
         print("admin user have been created successfully")
 
+    def _get_last_month(self):
+        "Returns random date in last month"
+        today = timezone.now()
+        first_month_day = today.replace(day=1)
+        last_month = first_month_day - timedelta(days=1)
+        return datetime(last_month.year,
+                        last_month.month,
+                        random.randint(1, 28),
+                        tzinfo=pytz.utc)
+
+    def _get_this_year(self):
+        "Returns random date in this year"
+        today = timezone.now()
+        return datetime(today.year,
+                        random.randint(1, today.month),
+                        random.randint(1, today.day),
+                        tzinfo=pytz.utc)
+
+    def _get_all_time(self):
+        "Returns random date"
+        today = timezone.now()
+        return datetime(random.randint(2000, today.year-1),
+                        random.randint(1, 12),
+                        random.randint(1, 28),
+                        tzinfo=pytz.utc)
+
     def create_transactions(self):
+        categories = [Transaction.EXPENSE, Transaction.INCOME]
+
+        # create now
         TransactionFactory.create_batch(
-            10,
-            amount=factory.Sequence(lambda n: random.randrange(0, 10)),
-            category=random.choice([Transaction.EXPENSE, Transaction.INCOME]),
+            5,
+            amount=factory.Sequence(lambda n: random.randint(1, 10)),
+            category=factory.Sequence(lambda n: random.choice(categories)),
             user=self.admin,
+        )
+
+        # create for last month
+        TransactionFactory.create_batch(
+            5,
+            amount=factory.Sequence(lambda n: random.randint(1, 10)),
+            category=factory.Sequence(lambda n: random.choice(categories)),
+            user=self.admin,
+            created=factory.Sequence(lambda n: self._get_last_month()),
+        )
+
+        # create for this year
+        TransactionFactory.create_batch(
+            5,
+            amount=factory.Sequence(lambda n: random.randint(1, 10)),
+            category=factory.Sequence(lambda n: random.choice(categories)),
+            user=self.admin,
+            created=factory.Sequence(lambda n: self._get_this_year()),
+        )
+
+        # create for all time
+        TransactionFactory.create_batch(
+            5,
+            amount=factory.Sequence(lambda n: random.randint(1, 10)),
+            category=factory.Sequence(lambda n: random.choice(categories)),
+            user=self.admin,
+            created=factory.Sequence(lambda n: self._get_all_time()),
         )
         print("Transactions for admin created")
