463b5f6e41e105199d35b670521fe1a0fb050b3d
Justas Trimailovas <j.trimailovas@gmail.com>
2016-07-09 12:51:11 +0300

    If not logged in redirect straight to log in page

--- finance/views.py
+++ finance/views.py
@@ -1,9 +1,8 @@
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
-from django.shortcuts import render
 
 
 def home(request):
     if request.user.is_authenticated():
         return redirect(reverse('transaction_list'))
-    return render(request, 'index.html')
+    return redirect(reverse('login'))
