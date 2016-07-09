3d4f51fff2b7c8365d66bd44aeb2fad75f8ecadc
Justas Trimailovas <j.trimailovas@gmail.com>
2016-07-09 14:18:43 +0300

    Unnecessary code

--- books/forms.py
+++ books/forms.py
@@ -7,6 +7,3 @@ class TransactionForm(forms.ModelForm):
     class Meta:
         model = models.Transaction
         fields = ['title', 'amount', 'category']
-
-    def full_clean(self, *args, **kwargs):
-        super().full_clean(*args, **kwargs)
