f042907a722dfc6b255b85aa56c6d2aa31e87894
Justas Trimailovas <j.trimailovas@gmail.com>
2016-10-12 22:13:56 +0300

    Show custom templates for password reset views

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,14 +9,28 @@ from accounts import views
 
 
 urlpatterns = [
+    # login/logout
     url(r'^logout/$', auth_logout, name='logout'),
     url(r'^login/$', views.login, name='login'),
     url(r'^login_redirect/$', views.login_redirect, name='login_redirect'),
-    url(r'^reset/$', password_reset, name='password_reset'),
-    url(r'^reset-done/$', password_reset_done, name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+
+    # password reset
+    url(r'^password-reset/$',
+        password_reset,
+        {'template_name': 'password_reset.html'},
+        name='password_reset'),
+    url(r'^password-reset-done/$',
+        password_reset_done,
+        {'template_name': 'password_reset_done.html'},
+        name='password_reset_done'),
+    url(r'^password-reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         password_reset_confirm,
+        {'template_name': 'password_reset_confirm.html'},
         name='password_reset_confirm'),
-    url(r'^reset-complete/$', password_reset_complete, name='password_reset_complete'),
+    url(r'^password-reset-complete/$',
+        password_reset_complete,
+        {'template_name': 'password_reset_complete.html'},
+        name='password_reset_complete'),
+
     url(r'^', include('registration.backends.simple.urls')),
 ]
